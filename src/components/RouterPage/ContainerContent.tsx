f2ac9b578bb8c5c7e119124a5701c48991ea7e1f
BlackUserSide <nesmserg@gmail.com>
2021-06-21 18:07:49 +0300

    routers

---
+++ src/components/RouterPage/ContainerContent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+type TProps = {
+  category: number;
+};
+export const ContainerContent: React.FC<TProps> = ({ category }) => {
+  return <div className="container-content"></div>;
+};
